215137c52acbf67075a12a7a8153dd9eacfde867
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-12-15 17:15:58 -0600

    Updated projects page. Added content to Contact page. Included links to github, linkedin, email, and resume. Changed web tab icon.

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,17 @@ const Contact = () => {
 
     return (
         <div id="Contact" className="contact-container">
-            <h1>contact</h1>
+            <h1 className="contact-header">Contact</h1>
+            <h2 className="contact-content">I love connecting with others interested in tech, so please do not hesitate to reach out! The best ways to contact me are below.</h2>
+            <div className="icon-container">
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/kevin-mcmahon-dev/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://github.com/kevin-mcmahon-dev" target="_blank"><i class="fab fa-github"></i></a></li>
+                    {/* <li><i class="fab fa-facebook"></i></li> */}
+                    <li><a href="mailto: kevinmcm1229@gmail.com" target="_blank"><i class="far fa-envelope"></i></a></li>
+                    <li><a href="https://github.com/kevin-mcmahon-dev/Portfolio/files/7722499/Kevin.McMahon.-.Resume.Dec.2021.pdf" target="_blank"><i class="far fa-file"></i></a></li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/Projects.js
+++ src/Projects.js
@@ -4,30 +4,31 @@ const Projects = () => {
 
     return (
         <div id="Projects" className="projects-container">
-            <h1 className="SectionHeader">Projects</h1>
-            <section>
-                <img src="https://user-images.githubusercontent.com/75190250/146046952-31b8ca87-7378-462e-b36c-6129d882a40c.png" alt="Dodge the Boulders image"></img>
-                <h1>Dodge the Boulders</h1>
-                <h2><a href="https://kevin-mcmahon-dev.github.io/dodge-the-boulders/">Live</a> | <a href="https://github.com/kevin-mcmahon-dev/dodge-the-boulders">Code</a> </h2>
-                <h2>HTML, CSS, Javascript</h2>
-                <ul>
-                    <li>Developed game in which the player must move from start zone to end zone while avoiding moving obstacles</li>
-                    <li>Created game structure with HTML and CSS Grid and animated moving blocks by using JavaScript with jQuery</li>
-                    <li>Utilized arrow-key inputs for player motion and implemented a 2D collision detection system for loss conditions</li>
-                </ul>
-                
+            <h1 className="section-header">Projects</h1>
+            <section className="project-section">
+                <img className="project-image" src="https://user-images.githubusercontent.com/75190250/146254098-4153b576-b4e9-4ff0-83f1-cc1e5bd87d7b.png" alt="Dodge the Boulders image"></img>
+                <div className="project-detail">   
+                    <h1 className="project-title">Dodge the Boulders - <a href="https://kevin-mcmahon-dev.github.io/dodge-the-boulders/" target="_blank">Live</a> | <a href="https://github.com/kevin-mcmahon-dev/dodge-the-boulders" target="_blank">Code</a> </h1>
+                    <h2 className="tech-list">HTML, CSS, Javascript</h2>
+                    <ul>
+                        <li>Developed game in which the player must move from start zone to end zone while avoiding moving obstacles</li>
+                        <li>Created game structure with HTML and CSS Grid and animated moving blocks by using JavaScript with jQuery</li>
+                        <li>Utilized arrow-key inputs for player motion and implemented a 2D collision detection system for loss conditions</li>
+                    </ul>
+                </div>
             </section>
-            <section>
-            <img src="https://user-images.githubusercontent.com/75190250/146048162-670684b7-c5df-4748-a11e-22e63741f5e5.png" alt="Sell-Books-Up Image"></img>
-            <h1>Sell-Books-Up</h1>
-            <h2><a href="https://sell-books-up.herokuapp.com/">Live</a> | <a href="https://github.com/kevin-mcmahon-dev/Sell-Books-Up">Code</a> </h2>
-            <h2>JavaScript, Node, Express, MongoDB, Mongoose</h2>
-            <ul>
-                <li>Collaborated with two other developers to create a MEN Stack application that allows users to review books</li>
-                <li>Individually focused on CRUD functionality, route structure, user authentication, and app connection with back-end</li>
-                <li>Full CRUD functionality accessible only through Admin account, standard users can access book review form</li>
-                <li>Established cloud database using Mongo Atlas and successfully deployed the application with Heroku</li>
-            </ul>
+            <section className="project-section">
+            <img className="project-image" src="https://user-images.githubusercontent.com/75190250/146048162-670684b7-c5df-4748-a11e-22e63741f5e5.png" alt="Sell-Books-Up Image"></img>
+            <div className="project-detail">     
+                <h1 className="project-title">Sell-Books-Up - <a href="https://sell-books-up.herokuapp.com/" target="_blank">Live</a> | <a href="https://github.com/kevin-mcmahon-dev/Sell-Books-Up" target="_blank">Code</a></h1>
+                <h2 className="tech-list">JavaScript, Node, Express, MongoDB, Mongoose</h2>
+                <ul>
+                    <li>Collaborated with two other developers to create a MEN Stack application that allows users to review books</li>
+                    <li>Individually focused on CRUD functionality, route structure, user authentication, and app connection with back-end</li>
+                    <li>Full CRUD functionality accessible only through Admin account, standard users can access book review form</li>
+                    <li>Established cloud database using Mongo Atlas and successfully deployed the application with Heroku</li>
+                </ul>
+            </div>
             </section>
         </div>
     );
